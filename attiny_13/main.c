4cd7d25a6b6dc102a20d33df9a25a369e62fff2f
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2022-05-18 23:06:48 +0200

    attiny_13: testing TCNT timer

--- attiny_13/main.c
+++ attiny_13/main.c
@@ -8,22 +8,21 @@ void blink(uint8_t b); // blink sequence function
 int main(void){
 	DDRB |= 1<<LED; // led pin as output
 	PORTB |= 1<<BUTTON; // button pin input pullup
-uint8_t i = 20; // blinks in sequence
+	TCCR0B |= 1<<CS00;
+	int repeatCount = 0;
+
 	while(1){
-if(!(PINB & 1<<BUTTON)){ // if button pushed call blink function
-	blink(i);
+if( TCNT0>251){ // if button pushed call blink function
+//	TCNT0 = 0;
+	repeatCount++;
+
+	if(repeatCount>8000){
+		PORTB ^= 1<<LED;
+		repeatCount = 0;
+		}
+
+		}
 	}
-
-
-
-			}
-
 }
-void blink(uint8_t b){
-	for(uint8_t x=0; x<b; x++){
-					PORTB ^= 1<<LED;
-					_delay_ms(60);
-				};
-	PORTB &=~(1<<LED);
-}
+
 
