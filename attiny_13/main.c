98e95dc8df6d3ba0043e866baa5cbec5b298b2ad
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2022-05-18 21:46:48 +0200

    attiny_13: blink sequence when button pushed

--- attiny_13/main.c
+++ attiny_13/main.c
@@ -1,24 +1,29 @@
 #include <avr/io.h>
 #include <util/delay.h>
-void blink(uint8_t b);
-int main(void){
-	DDRB |= 1<<PB3;
-	PORTB |= 1<<PB4;
-uint8_t i = 15;
-	while(1){
-		if(!(PINB & 1<<PB4) ){
-			blink(i);
-			_delay_ms(300);
-		}
-		else{PORTB &=~(1<<PB3);}
+#define LED PB3
+#define BUTTON PB4
 
+void blink(uint8_t b); // blink sequence function
+
+int main(void){
+	DDRB |= 1<<LED; // led pin as output
+	PORTB |= 1<<BUTTON; // button pin input pullup
+uint8_t i = 20; // blinks in sequence
+	while(1){
+if(!(PINB & 1<<BUTTON)){ // if button pushed call blink function
+	blink(i);
 	}
+
+
+
+			}
 
 }
 void blink(uint8_t b){
-	for(uint8_t x=b; x>=0; x--){
-					PORTB ^= 1<<PB3;
-					_delay_ms(150);
+	for(uint8_t x=0; x<b; x++){
+					PORTB ^= 1<<LED;
+					_delay_ms(60);
 				};
+	PORTB &=~(1<<LED);
 }
 
