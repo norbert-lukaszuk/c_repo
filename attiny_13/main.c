1092907f3c0c3e9764a29f5a7b8317eafa0a281b
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2022-05-19 11:59:11 +0200

    attiny_13: use of 1024 prescaler for timer

--- attiny_13/main.c
+++ attiny_13/main.c
@@ -3,20 +3,21 @@
 #define LED PB3
 #define BUTTON PB4
 
-void blink(uint8_t b); // blink sequence function
-
 int main(void){
 	DDRB |= 1<<LED; // led pin as output
 	PORTB |= 1<<BUTTON; // button pin input pullup
 	TCCR0B |= 1<<CS00;
+	TCCR0B |= 1<<CS02;
+
 	int repeatCount = 0;
 
 	while(1){
-if( TCNT0>251){ // if button pushed call blink function
-//	TCNT0 = 0;
+if( TCNT0>250){ // if button pushed call blink function
+	TCNT0 = 0;
 	repeatCount++;
+//	PORTB ^= 1<<LED;
 
-	if(repeatCount>8000){
+	if(repeatCount>32){
 		PORTB ^= 1<<LED;
 		repeatCount = 0;
 		}
