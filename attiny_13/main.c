204b5ceb3e31766a2cf63a2902f7d83040eb3e92
norbert-lukaszuk <norbert.lukaszuk@gmail.com>
2022-05-20 22:41:04 +0200

    attiny13: trying interrupts

--- attiny_13/main.c
+++ attiny_13/main.c
@@ -3,27 +3,41 @@
 #define LED PB3
 #define BUTTON PB4
 
+uint8_t repeatCount;
+void toggle_led(void);
 int main(void){
+	sei();
 	DDRB |= 1<<LED; // led pin as output
-	PORTB |= 1<<BUTTON; // button pin input pullup
-	TCCR0B |= 1<<CS00;
-	TCCR0B |= 1<<CS02;
+	PORTB |= 1<<BUTTON; // button pin input pull up
+	TCCR0B |= 1<<CS00 | 1<<CS02 ; //set timer prescaler for 1024
+	TCCR0A |= 1<<WGM01;
+	OCR0A = 250;
+	TIMSK0 |= 1<<OCIE0A;
 
-	int repeatCount = 0;
+
 
 	while(1){
-if( TCNT0>250){ // if button pushed call blink function
-	TCNT0 = 0;
-	repeatCount++;
-//	PORTB ^= 1<<LED;
-
-	if(repeatCount>64){
-		PORTB ^= 1<<LED;
-		repeatCount = 0;
-		}
-
-		}
+//if( TCNT0>250){ // if button pushed call blink function
+//	TCNT0 = 0;
+//	repeatCount++;
+//
+//
+//	if(repeatCount>38){
+//		PORTB ^= 1<<LED;
+//		repeatCount = 0;
+//		}
+//
+//		}
 	}
 }
-
+void toggle_led(void){
+	repeatCount++;
+		if(repeatCount>38){
+				PORTB ^= 1<<LED;
+				repeatCount = 0;
+				}
+}
+ISR(TIM0_COMPA_vect){
+//	PORTB ^= 1<<LED;
+}
 
